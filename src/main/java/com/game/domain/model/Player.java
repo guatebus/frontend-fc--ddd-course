e1284e22630f68bc6f8a7fe281d022754ed52c7e
Ivan Lorenz <ivan.lorenz@scmspain.com>
2017-05-26 10:04:43 +0200

    Added Player domain object

---
+++ src/main/java/com/game/domain/model/Player.java
@@ -0,0 +1,43 @@
+package com.game.domain.model;
+
+public class Player {
+    private final String name;
+    private int hitPoints = 10;
+    private int attackPoints = 2;
+    private int defensePoints = 2;
+    private int level = 1;
+    private int experience = 0;
+    private int gold = 0;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints;
+    }
+
+    public int getDefensePoints() {
+        return defensePoints;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+}
---
+++ src/test/java/PlayerSpecification.java
@@ -0,0 +1,20 @@
+import com.game.domain.model.Player;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerSpecification {
+
+    @Test
+    public void should_have_initial_attributes() throws Exception {
+        Player player = new Player("A_Player");
+        assertEquals(player.getName(),"A_Player");
+        assertEquals(player.getHitPoints(), 10);
+        assertEquals(player.getAttackPoints(), 2);
+        assertEquals(player.getDefensePoints(), 2);
+        assertEquals(player.getLevel(), 1);
+        assertEquals(player.getExperience(), 0);
+        assertEquals(player.getGold(), 0);
+    }
+}
